9ea64347b6fcfc96162736f7f5ae5009aa8d830f
takayuki-hara <takayuki.hara.dev@gmail.com>
2018-07-14 13:13:08 +0900

    Wrapのサンプルを追加

--- json_sample/json-sample/ViewController.swift
+++ json_sample/json-sample/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
 
         UnboxSample.test()
+        WrapSample.test()
     }
 
     override func didReceiveMemoryWarning() {
--- json_sample/json-sample/WrapSample.swift
+++ json_sample/json-sample/WrapSample.swift
@@ -7,3 +7,85 @@
 //
 
 import Foundation
+import Wrap
+
+// -----------------------------------------------------------------------------
+
+struct User2 {
+    let name: String
+    let age: Int
+}
+
+let user = User2(name: "John", age: 28)
+
+// -----------------------------------------------------------------------------
+
+struct SpaceShip2 {
+    let type: SpaceShipType2
+    let weight: Double
+    let engine: Engine2
+    let passengers: [Astronaut2]
+    let launchLiveStreamURL: URL?
+    let lastPilot: Astronaut2?
+}
+
+enum SpaceShipType2: Int, WrappableEnum {
+    case apollo
+    case sputnik
+}
+
+struct Engine2 {
+    let manufacturer: String
+    let fuelConsumption: Float
+}
+
+struct Astronaut2 {
+    let name: String
+}
+
+let ship = SpaceShip2(
+    type: .apollo,
+    weight: 3999.72,
+    engine: Engine2(
+        manufacturer: "The Space Company",
+        fuelConsumption: 17.2321
+    ),
+    passengers: [
+        Astronaut2(name: "Mike"),
+        Astronaut2(name: "Amanda")
+    ],
+    launchLiveStreamURL: URL(string: "http://livestream.com"),
+    lastPilot: nil
+)
+
+// -----------------------------------------------------------------------------
+
+struct Book: WrapCustomizable {
+    let title: String
+    let authorName: String
+
+    func keyForWrapping(propertyNamed propertyName: String) -> String? {
+        if propertyName == "authorName" {
+            return "author_name"
+        }
+
+        return propertyName
+    }
+}
+
+let book = Book(title: "title", authorName: "author")
+
+// -----------------------------------------------------------------------------
+
+class WrapSample {
+    class func test() {
+        let userDic: [String : Any]? = try? wrap(user)
+        print(userDic ?? "error")
+
+        let shipDic: WrappedDictionary? = try? wrap(ship)
+        print(shipDic ?? "error")
+
+        let bookDic: WrappedDictionary? = try? wrap(book)
+        print(bookDic ?? "error")
+    }
+}
